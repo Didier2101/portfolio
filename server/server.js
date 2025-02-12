23a57e93a3ada9bcfdaec9edf9bcac322a5a3202
Didier Chávez <didierchavez2101@gmail.com>
2025-02-12 18:27:03 -0500

    Se3gundo commit6

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: ["https://portafolio.com"], // Cambia esto por tu dominio
+  origin: ["https://portfolio-gnkh.onrender.com"], // Cambia esto por tu dominio
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
