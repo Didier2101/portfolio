e6a286d3e43eaf56d7e5a633f279dbe80fec79c7
Didier Chávez <didierchavez2101@gmail.com>
2025-02-12 17:57:56 -0500

    Se3gundo commit3

--- server/server.js
+++ server/server.js
@@ -10,7 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: ["https://miportafolio.com"], // Cambia esto por tu dominio
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // Configurar Nodemailer
